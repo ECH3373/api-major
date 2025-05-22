83fd0657d8d16aae83e2006f30cfc70fec6d6fcc
37r4n <e.37r4n@gmail.com>
2025-05-22 11:29:06 -0600

    fix

--- src/dot/controller.js
+++ src/dot/controller.js
@@ -3,29 +3,29 @@ import { services } from '../shared/services/index.js';
 const expand = [{ key: 'employee_id', name: 'employee', endpoint: 'https://gateway.stullerlandcommunity.com/employees' }, { key: 'resource_id', name: 'resource', endpoint: 'https://gateway.stullerlandcommunity.com/courses/resources' }];
 
 const index = async (req, res) => {
-  const { data, meta } = await services.crud.index({ model: 'piece', query: req.query, expand, filters: ['resource_id', 'employee_id'] });
-  return services.response.send({ res, data, meta, message: 'the list of pieces has been successfully retrieved' });
+  const { data, meta } = await services.crud.index({ model: 'dot', query: req.query, expand, filters: ['defect_id', 'piece_id'] });
+  return services.response.send({ res, data, meta, message: 'the list of dots has been successfully retrieved' });
 };
 
 const show = async (req, res) => {
-  const data = await services.crud.show({ model: 'piece', target: req.params.id, expand });
-  if (!data) return services.response.send({ res, data, error: 'the piece with the provided ID does not exist' });
-  services.response.send({ res, data, message: 'piece successfully retrieved' });
+  const data = await services.crud.show({ model: 'dot', target: req.params.id, expand });
+  if (!data) return services.response.send({ res, data, error: 'the dot with the provided ID does not exist' });
+  services.response.send({ res, data, message: 'dot successfully retrieved' });
 };
 
 const store = async (req, res) => {
-  const data = await services.crud.store({ model: 'piece', payload: req.body, keys: ['resource_id', 'employee_id'] });
-  return services.response.send({ res, data, message: 'piece created successfully' });
+  const data = await services.crud.store({ model: 'dot', payload: req.body, keys: ['position_x', 'position_y', 'defect_id', 'piece_id'] });
+  return services.response.send({ res, data, message: 'dot created successfully' });
 };
 
 const update = async (req, res) => {
-  const data = await services.crud.update({ model: 'piece', id: req.params.id, payload: req.body });
-  return services.response.send({ res, data, message: 'piece updated successfully' });
+  const data = await services.crud.update({ model: 'dot', id: req.params.id, payload: req.body });
+  return services.response.send({ res, data, message: 'dot updated successfully' });
 };
 
 const destroy = async (req, res) => {
-  const data = await services.crud.destroy({ model: 'piece', id: req.params.id });
-  return services.response.send({ res, data, message: 'piece deleted successfully' });
+  const data = await services.crud.destroy({ model: 'dot', id: req.params.id });
+  return services.response.send({ res, data, message: 'dot deleted successfully' });
 };
 
 export const controller = {
--- src/enrollment/controller.js
+++ src/enrollment/controller.js
@@ -4,29 +4,29 @@ const include = [{ key: 'event_id', name: 'event' }];
 const expand = [{ key: 'defect_id', name: 'defect', endpoint: 'https://gateway.stullerlandcommunity.com/defects' }, { key: 'piece_id', name: 'piece', endpoint: 'https://gateway.stullerlandcommunity.com/courses/resources' }];
 
 const index = async (req, res) => {
-  const { data, meta } = await services.crud.index({ model: 'dot', query: req.query, expand, include, search: ['employee_id', 'event_id'], filters: ['employee_id', 'event_id'] });
-  return services.response.send({ res, data, meta, message: 'the list of dots has been successfully retrieved' });
+  const { data, meta } = await services.crud.index({ model: 'enrollment', query: req.query, expand, include, search: ['employee_id', 'event_id'], filters: ['employee_id', 'event_id'] });
+  return services.response.send({ res, data, meta, message: 'the list of enrollments has been successfully retrieved' });
 };
 
 const show = async (req, res) => {
-  const data = await services.crud.show({ model: 'dot', target: req.params.id, expand });
-  if (!data) return services.response.send({ res, data, error: 'the dot with the provided ID does not exist' });
-  services.response.send({ res, data, message: 'dot successfully retrieved' });
+  const data = await services.crud.show({ model: 'enrollment', target: req.params.id, expand });
+  if (!data) return services.response.send({ res, data, error: 'the enrollment with the provided ID does not exist' });
+  services.response.send({ res, data, message: 'enrollment successfully retrieved' });
 };
 
 const store = async (req, res) => {
-  const data = await services.crud.store({ model: 'dot', payload: req.body, keys: ['employee_id', 'event_id'] });
-  return services.response.send({ res, data, message: 'dot created successfully' });
+  const data = await services.crud.store({ model: 'enrollment', payload: req.body, keys: ['employee_id', 'event_id'] });
+  return services.response.send({ res, data, message: 'enrollment created successfully' });
 };
 
 const update = async (req, res) => {
-  const data = await services.crud.update({ model: 'dot', id: req.params.id, payload: req.body });
-  return services.response.send({ res, data, message: 'dot updated successfully' });
+  const data = await services.crud.update({ model: 'enrollment', id: req.params.id, payload: req.body });
+  return services.response.send({ res, data, message: 'enrollment updated successfully' });
 };
 
 const destroy = async (req, res) => {
-  const data = await services.crud.destroy({ model: 'dot', id: req.params.id });
-  return services.response.send({ res, data, message: 'dot deleted successfully' });
+  const data = await services.crud.destroy({ model: 'enrollment', id: req.params.id });
+  return services.response.send({ res, data, message: 'enrollment deleted successfully' });
 };
 
 export const controller = {
